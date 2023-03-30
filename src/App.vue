87e8798767435f975dffb9cb72f6635851fe343d
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-03-30 12:46:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import AppNavbar from './components/AppNavbar.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: {
+    AppNavbar,
+    AppHeader,
+    AppMain,
+  },
+
+}
+
+</script>
+
+
+<template>
+  <div>
+
+    <AppNavbar></AppNavbar>
+
+    <AppHeader></AppHeader>
+
+    <AppMain></AppMain>
+    
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+import {store} from "../store.js";
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+    
+</style>
